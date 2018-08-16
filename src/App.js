d299ce3abd9305f232fdad572f3be976c2f0ee60
Lauren Perchitti <31042804+Perchitti@users.noreply.github.com>
2018-08-15 23:22:03 -0400

    removed logo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { Provider } from "react-redux";
+import { Provider } from "react-redux"; //react component it is the glue for React/Redux
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./components/home";
 import About from "./components/about";
 import News from "./components/news";
 import Navbar from "./components/customNavBar";
 
+
 import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
+      <Provider store={store}>  
         <div>
           <header>
             <Router>
@@ -31,5 +31,8 @@ class App extends Component {
     );
   }
 }
-
+//provider makes the entire redux store available to components in the heirarchy
 export default App;
+
+//redux-thunk allows us to access the dispatch method so that we can make asynchronous calls from our actions
+//
