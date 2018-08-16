14dd46a7c890d9542e4302e906bfeb39eb72cfda
Lauren Perchitti <31042804+Perchitti@users.noreply.github.com>
2018-08-15 23:21:09 -0400

    added stateless functions

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+import PropTypes from 'prop-types'; //
+import { connect } from 'react-redux'; //connects components to redux store that was provided by provider component
 import { fetchPosts } from '../actions/postActions';
 
 class Posts extends Component {
-  componentWillMount() {
+
+  componentWillMount = () => {
     this.props.fetchPosts();
   }
+  // when it mounts make request to API to fetch post
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.newPost) {
-      this.props.posts.unshift(nextProps.newPost);
+  componentWillReceiveProps = (nextProps) => {
+    if (nextProps.newPost) { //we are checking to see if newPost is there
+      this.props.posts.unshift(nextProps.newPost); //unshift adds to the beginning
     }
   }
+  //when this recieves a new prop from the state it will run ^
+  //
 
   render() {
+    //will loops through our API to grab this information
     const postItems = this.props.posts.map(post => (
       <div key={post.id}>
         <h3>{post.title}</h3>
@@ -26,6 +31,7 @@ class Posts extends Component {
         <h1>Posts</h1>
         {postItems}
       </div>
+      //this post title & body to the page
     );
   }
 }
@@ -36,9 +42,10 @@ Posts.propTypes = {
   newPost: PropTypes.object
 };
 
-const mapStateToProps = state => ({
-  posts: state.posts.items,
-  newPost: state.posts.item
+const mapStateToProps = state => ({ //getting new items from the state and using it in our component, mapped the items in the state to the posts property
+  posts: state.posts.items, //posts, refers to our rootreducer in reducer/index.js
+  newPost: state.posts.item //this comes from our reducer
 });
 
 export default connect(mapStateToProps, { fetchPosts })(Posts);
+//
